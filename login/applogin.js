d258cc10d930b783d98cc5009585022c08b830c7
Mahesh Rathi <rmaheshrathi115@gmail.com>
2022-08-02 03:17:09 +0530

    Initial commit

---
+++ login/applogin.js
@@ -0,0 +1,8 @@
+
+    if (! localStorage.getItem('user') ) {
+        document.getElementById('signup').style.display = 'block';
+        document.getElementById('signout').style.display = 'none';
+    } else {
+        document.getElementById('signup').style.display = 'none';
+        document.getElementById('signout').style.display = 'block';
+    }
